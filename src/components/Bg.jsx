9ca4b2f8edadd0e6c057ee1c5f29d9c78025d8b5
srthkw <srthk44@gmail.com>
2026-01-10 12:23:33 +0530

    Events bg change for mobile view

--- src/components/Bg.jsx
+++ src/components/Bg.jsx
@@ -4,7 +4,7 @@ import bg from '../assets/bg.jpg'
 const Bg = () => {
   return (
     <div>
-            <div className="fixed inset-0 bg-black -z-50"></div>
+            <div className="fixed inset-0 bg-black -z-50 w-full h-full"></div>
             <div
               className="fixed inset-0 bg-cover bg-center opacity-85 z-0"
               style={{ backgroundImage: `url(${bg})` }}

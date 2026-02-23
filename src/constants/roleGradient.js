236ddf778520cf9586373cb225b8d38a12b6a474
srthkw <srthk44@gmail.com>
2026-02-23 23:10:34 +0530

    .

--- src/constants/roleGradient.js
+++ src/constants/roleGradient.js
@@ -1,6 +1,5 @@
 export const roleGradients = {
-  attendee: 'from-blue-200 via-blue-100 to-gray-200',
-  organizer: 'from-emerald-100 via-emerald-100 to-gray-300',
-  admin: 'from-purple-200 via-violet-100 to-gray-200'
+  attendee: 'from-blue-200 via-blue-100 to-blue-100',
+  organizer: 'from-emerald-200 via-emerald-100 to-emerald-100',
+  admin: 'from-violet-200 via-violet-100 to-violet-100'
 };
-

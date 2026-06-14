03638f2b6fc59f6149e6ab84a97a842e10bc3ede
srthkw <srthk44@gmail.com>
2026-06-14 20:12:02 +0530

    .

--- src/pages/CreateEvent.jsx
+++ src/pages/CreateEvent.jsx
@@ -62,7 +62,7 @@ const CreateEvent = () => {
         }
     };
 
-    if (!user || user.role !== "admin") {
+    if (user.role !== "organizer" || user.role !== "admin") {
         return (
             <div className={`min-h-screen bg-gradient-to-br ${roleGradientsBG[user.role] || ''} flex flex-col items-center text-gray-600`}>
                 <Navbar />
